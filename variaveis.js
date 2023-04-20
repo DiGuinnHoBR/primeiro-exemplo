27390b14ffa7bc1808ddaca4ffe065b38166cc40
Diogo Vieira <diogov770@gmail.com>
2023-04-20 20:20:46 -0300

    Adicionando mais de um arquivo

---
+++ variaveis.js
@@ -0,0 +1,45 @@
+// VARIÁVEIS
+
+let idade = 18; // criando uma variável com comando let
+console.log(idade);
+
+/*
+
+REGRAS DECLARAÇÃO DE VARIÁVEIS
+
+- Utilizar sempre “let” ou “const”
+- Não é permitido o uso de palavras reservadas
+- Não utilizar traço/espaço
+- Evitar o uso de acentuação/caracteres especiais
+- Não começar com números
+- Utilizar nomes significados
+- Utilizar camelCase
+
+*/
+
+// Tipos primitivos
+let quantidade = 20; // number
+let preco = 99.9; // number
+let produto = 'Mouse'; // string (texto)
+let disponivel = false; // boolean (true or false)
+
+console.log(quantidade, typeof quantidade);
+console.log(preco, typeof preco);
+console.log(produto, typeof produto);
+console.log(disponivel, typeof disponivel);
+
+let curso;
+console.log(curso);
+curso = 'JavaScript';
+console.log(curso);
+
+let nula = null;
+console.log(nula);
+
+// Declaração de variáveis
+var variavel = 'Boa noite!'; // Evitar o uso
+console.log(variavel);
+
+const valorFixo = 100;
+console.log(valorFixo);
+// valorFixo = 200; => Não permite reatribuição
